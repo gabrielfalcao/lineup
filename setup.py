1b49a6b142f4df1be650670903d2515f5de7c75f
Gabriel Falcao <gabriel@nacaolivre.org>
2014-02-02 09:38:39 -0500

    fixing cli entry point

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
         include_package_data=True,
         entry_points={
             'console_scripts': [
-                'lineup = lineup.tool:main',
+                'lineup = lineup.cli:main',
             ]
         },
         classifiers=[
