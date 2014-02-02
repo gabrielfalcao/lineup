162b480d0fd55b81582bcf1cc9a4dd3b3d070f90
Gabriel Falcao <gabriel@nacaolivre.org>
2014-02-02 08:37:26 -0500

    fixing tests

--- tests/functional/base.py
+++ tests/functional/base.py
@@ -2,18 +2,21 @@
 # -*- coding: utf-8 -*-
 #
 from __future__ import unicode_literals
-
 from sure import scenario
-from redis import StrictRedis
+from lineup import JSONRedisBackend
+
 from threading import RLock
 
 
 test_lock = RLock()
 
+
 def prepare_redis(context):
-    context.redis = StrictRedis()
+    context.backend = JSONRedisBackend()
+    context.redis = context.backend.redis
     context.redis.flushall()
     test_lock.acquire()
+
 
 def unlock(context):
     test_lock.release()
